4f40aaaf5d23be81ca02ff9eb0fe009b342510e4
mike2611 <mpuentes2611@gmail.com>
2022-02-07 13:46:56 -0600

    Add test for costgroup

---
+++ spec/models/cost_groups_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe CostGroup, type: :model do
+  context 'Testing validations for cost_groups' do
+    before(:all) do
+      @user = User.create(name: 'John Smith')
+      @cost = Cost.create(name: 'CostTest', amount: 2, user:  @user )
+      @group = Group.create(name: 'GroupTest', icon: 'http://icon.url', user:  @user )
+    end
+    subject { CostGroup.new(cost: @cost, group: @group )}
+    before { subject.save }
+    after(:all) do
+      @cost.destroy
+      @group.destroy
+      @user.destroy
+    end
+
+    it 'is valid with the inserted valid attributes' do
+      expect(subject).to be_valid
+    end
+  end
+end
