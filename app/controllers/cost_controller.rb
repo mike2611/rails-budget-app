8c462038bcf0a1bcdec896c02b8b1288e6d05a79
mike2611 <mpuentes2611@gmail.com>
2022-02-08 17:43:08 -0600

    Create transactions index

--- app/controllers/cost_controller.rb
+++ app/controllers/cost_controller.rb
@@ -1,3 +1,19 @@
 class CostController < ApplicationController
-  def index; end
+  load_and_authorize_resource
+  def index
+    group = Group.find(params[:group_id])
+    @costs = group.costs 
+    @total = total_costs
+  end
+
+  private 
+  
+  def total_costs
+    return {} if current_user.nil?
+    amount = 0
+    @costs.each do |cost|
+      amount += cost.amount
+    end
+    amount
+  end
 end
