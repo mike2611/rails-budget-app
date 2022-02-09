327e1e85bff39ea02c65076563deafa1a235f91e
mike2611 <mpuentes2611@gmail.com>
2022-02-09 12:19:31 -0600

    Add styles to log in and sign up page

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,11 @@
 # Examples:
 user = User.create(name:'mike', email:'mike@gmail.com' ,password:'mike123');
 group = Group.create(name:'Fast food', icon:'https://cdn-icons-png.flaticon.com/512/732/732060.png', user_id:user.id)
-cost = Cost.create(name:'hamburguer', amount: 500, user_id:user.id)
-cost2 = Cost.create(name:'fries', amount: 250, user_id:user.id)
-CostGroup.create(cost_id:cost.id, group_id:group.id)
-CostGroup.create(cost_id:cost2.id, group_id:group.id)
+group2 = Group.create(name:'Clothes', icon:'https://media.istockphoto.com/vectors/creative-clothes-logo-design-vector-id1148638300', user_id:user.id)
+cost = Cost.create(name:'Hamburguer', amount: 50, user_id:user.id)
+cost2 = Cost.create(name:'Fries', amount: 25, user_id:user.id)
+cost3 = Cost.create(name:'Hoodie', amount: 500, user_id:user.id)
+CostGroup.create(cost_id:cost.id, group:group)
+CostGroup.create(cost_id:cost2.id, group:group)
+CostGroup.create(cost_id:cost3.id, group:group2)
 
