a4ba851b2c9bd11c1db5190cd636dbec03b0e99c
mike2611 <mpuentes2611@gmail.com>
2022-02-09 16:07:24 -0600

    Add spec for views/transactions_index

---
+++ spec/views/transactions_index_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Transactions index', type: :system do
+  describe 'Check the content of transactions index' do
+    before(:each) do
+      user = User.create(name:'mike', email:'mike@gmail.com' ,password:'mike123');
+      group = Group.create(name:'Fast food', icon:'https://cdn-icons-png.flaticon.com/512/732/732060.png', user_id:user.id)
+      group2 = Group.create(name:'Clothes', icon:'https://media.istockphoto.com/vectors/creative-clothes-logo-design-vector-id1148638300', user_id:user.id)
+      cost = Cost.create(name:'Hamburguer', amount: 50, user_id:user.id)
+      cost2 = Cost.create(name:'Fries', amount: 25, user_id:user.id)
+      cost3 = Cost.create(name:'Hoodie', amount: 500, user_id:user.id)
+      CostGroup.create(cost_id:cost.id, group:group)
+      CostGroup.create(cost_id:cost2.id, group:group)
+      CostGroup.create(cost_id:cost3.id, group:group2)      
+      visit root_path
+      sleep(2)
+      fill_in 'user_email', with: 'mike@gmail.com'
+      fill_in 'user_password', with: 'mike123'
+      click_button 'Log in'
+      sleep(2)
+      click_link 'Fast food'
+    end
+    it 'it shows the name for both transactions' do
+      expect(page).to have_content('Hamburguer')
+      expect(page).to have_content('Fries')
+    end
+    it 'it shows the correct amount for both transactions' do
+        expect(page).to have_content('50')
+        expect(page).to have_content('25')
+      end  
+    it 'it shows the Add Category btn' do
+      expect(page).to have_content('Add Transaction')
+    end   
+  end
+end
