0087c6693e5a603ac4adfe42457f43c8aad55d99
mike2611 <mpuentes2611@gmail.com>
2022-02-09 11:06:22 -0600

    Add functionality to costs/new.html.erb

--- app/controllers/cost_controller.rb
+++ app/controllers/cost_controller.rb
@@ -10,7 +10,32 @@ class CostController < ApplicationController
     @groups = Group.all
   end
 
+  def create
+    @cost.user = current_user
+    groups = params[:groups]
+
+    if groups.nil?
+      flash[:alert] = 'You should select at least one category'
+      redirect_to new_group_cost_path
+      @cost.destroy
+    else
+      groups.each do |key, value|
+        CostGroup.create(cost:@cost, group:Group.find(key))
+      end
+      if @cost.save
+        redirect_to root_path, notice: 'Succesfully created new transaction'
+      else
+        flash[:alert] = 'Error creating new transaction'
+        render :new
+      end
+    end
+  end
+
   private 
+
+  def cost_params
+    params.require(:cost).permit(:name, :amount, :groups)
+  end
   
   def total_costs
     return {} if current_user.nil?
