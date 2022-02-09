c27bfa968b595886405ce8a2ed190d8a9be2e23e
mike2611 <mpuentes2611@gmail.com>
2022-02-09 16:57:19 -0600

    Fix linter errors

--- spec/views/categories_index_spec.rb
+++ spec/views/categories_index_spec.rb
@@ -3,15 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Categories index', type: :system do
   describe 'Check the content of categories index' do
     before(:each) do
-      user = User.create(name:'mike', email:'mike@gmail.com' ,password:'mike123');
-      group = Group.create(name:'Fast food', icon:'https://cdn-icons-png.flaticon.com/512/732/732060.png', user_id:user.id)
-      group2 = Group.create(name:'Clothes', icon:'https://media.istockphoto.com/vectors/creative-clothes-logo-design-vector-id1148638300', user_id:user.id)
-      cost = Cost.create(name:'Hamburguer', amount: 50, user_id:user.id)
-      cost2 = Cost.create(name:'Fries', amount: 25, user_id:user.id)
-      cost3 = Cost.create(name:'Hoodie', amount: 500, user_id:user.id)
-      CostGroup.create(cost_id:cost.id, group:group)
-      CostGroup.create(cost_id:cost2.id, group:group)
-      CostGroup.create(cost_id:cost3.id, group:group2)      
+      user = User.create(name: 'mike', email: 'mike@gmail.com', password: 'mike123')
+      group = Group.create(name: 'Fast food', icon: 'exampleIcon',
+                           user_id: user.id)
+      group2 = Group.create(name: 'Clothes',
+                            icon: 'exampleIcon', user: user.id)
+      cost = Cost.create(name: 'Hamburguer', amount: 50, user_id: user.id)
+      cost2 = Cost.create(name: 'Fries', amount: 25, user_id: user.id)
+      cost3 = Cost.create(name: 'Hoodie', amount: 500, user_id: user.id)
+      CostGroup.create(cost_id: cost.id, group: group)
+      CostGroup.create(cost_id: cost2.id, group: group)
+      CostGroup.create(cost_id: cost3.id, group: group2)
       visit root_path
       sleep(2)
       fill_in 'user_email', with: 'mike@gmail.com'
@@ -22,13 +24,13 @@ RSpec.describe 'Categories index', type: :system do
     it 'it shows the name for both categories' do
       expect(page).to have_content('Fast food')
       expect(page).to have_content('Clothes')
-    end 
+    end
     it 'it shows the correct total for both categories' do
       expect(page).to have_content('75')
       expect(page).to have_content('500')
-    end  
+    end
     it 'it shows the Add Category btn' do
       expect(page).to have_content('Add Category')
-    end   
+    end
   end
 end
--- spec/views/transactions_new._spec.rb
+++ spec/views/transactions_new._spec.rb
@@ -3,15 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Transactions new', type: :system do
   describe 'Check the content of transactions index' do
     before(:each) do
-      user = User.create(name:'mike', email:'mike@gmail.com' ,password:'mike123');
-      group = Group.create(name:'Fast food', icon:'https://cdn-icons-png.flaticon.com/512/732/732060.png', user_id:user.id)
-      group2 = Group.create(name:'Clothes', icon:'https://media.istockphoto.com/vectors/creative-clothes-logo-design-vector-id1148638300', user_id:user.id)
-      cost = Cost.create(name:'Hamburguer', amount: 50, user_id:user.id)
-      cost2 = Cost.create(name:'Fries', amount: 25, user_id:user.id)
-      cost3 = Cost.create(name:'Hoodie', amount: 500, user_id:user.id)
-      CostGroup.create(cost_id:cost.id, group:group)
-      CostGroup.create(cost_id:cost2.id, group:group)
-      CostGroup.create(cost_id:cost3.id, group:group2)      
+      user = User.create(name: 'mike', email: 'mike@gmail.com', password: 'mike123')
+      group = Group.create(name: 'Fast food', icon: 'exampleIcon',
+                           user_id: user.id)
+      group2 = Group.create(name: 'Clothes',
+                            icon: 'exampleIcon', user_id: user.id)
+      cost = Cost.create(name: 'Hamburguer', amount: 50, user_id: user.id)
+      cost2 = Cost.create(name: 'Fries', amount: 25, user_id: user.id)
+      cost3 = Cost.create(name: 'Hoodie', amount: 500, user_id: user.id)
+      CostGroup.create(cost_id: cost.id, group: group)
+      CostGroup.create(cost_id: cost2.id, group: group)
+      CostGroup.create(cost_id: cost3.id, group: group2)
       visit root_path
       sleep(2)
       fill_in 'user_email', with: 'mike@gmail.com'
@@ -23,12 +25,12 @@ RSpec.describe 'Transactions new', type: :system do
       click_link 'Add Transaction'
     end
     it 'it shows the correct inputs' do
-        expect(page).to have_content('Name')
-        expect(page).to have_content('Amount')
+      expect(page).to have_content('Name')
+      expect(page).to have_content('Amount')
     end
     it 'it shows the correct checks' do
-        expect(page).to have_content('Fast food')
-        expect(page).to have_content('Clothes')
-    end      
+      expect(page).to have_content('Fast food')
+      expect(page).to have_content('Clothes')
+    end
   end
 end
